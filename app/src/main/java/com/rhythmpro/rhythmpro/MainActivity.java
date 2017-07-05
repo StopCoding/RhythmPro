c796f153a4a3c235bf5f8c8f3dc38cb16139c5c4
StopCoding <xiaoyanmiche@163.com>
2017-07-05 23:50:30 +0800

    update Gradle && manage buletooth

--- app/src/main/java/com/rhythmpro/rhythmpro/MainActivity.java
+++ app/src/main/java/com/rhythmpro/rhythmpro/MainActivity.java
@@ -1,15 +1,37 @@
 package com.rhythmpro.rhythmpro;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
 public class MainActivity extends baseActivity {
 
     private Button button;
     private BluetoothAdapter adapter;
+    private ArrayList mArrayAdapter;
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +54,38 @@ public class MainActivity extends baseActivity {
             String name = adapter.getName();
             String address = adapter.getAddress();
 
-            if (adapter.isDiscovering()) {
-                adapter.cancelDiscovery();
+            Set<BluetoothDevice> devices = adapter.getBondedDevices();
+            Iterator<BluetoothDevice> iterator = devices.iterator();
+
+            while (iterator.hasNext()) {
+                BluetoothDevice device = iterator.next();
+
+                String deviceName = device.getName();
+                String deviceAddress = device.getAddress();
+                int deviceType = device.getType();
             }
+
             adapter.startDiscovery();
 
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(receiver, filter);
         } else {
             Toast.makeText(this, "蓝牙未连接", Toast.LENGTH_SHORT).show();
+            //adapter.enable();
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, 0x1);
         }
+    }
+
+    public void registerFoundAction() {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+
+
+        super.onDestroy();
     }
 }
