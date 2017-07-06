c36cb4c42a80060b82ecf6a4a2ad3d795302cff4
JIANGLEI660 <jianglei660@pingan.com.cn>
2017-07-06 19:13:34 +0800

    listen buletooth

--- app/src/main/java/com/rhythmpro/rhythmpro/MainActivity.java
+++ app/src/main/java/com/rhythmpro/rhythmpro/MainActivity.java
@@ -1,19 +1,29 @@
 package com.rhythmpro.rhythmpro;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends baseActivity {
 
@@ -45,6 +55,10 @@ public class MainActivity extends baseActivity {
                 onButtonClick();
             }
         });
+
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
     }
 
     public void onButtonClick() {
@@ -65,10 +79,60 @@ public class MainActivity extends baseActivity {
                 int deviceType = device.getType();
             }
 
-            adapter.startDiscovery();
+            //adapter.startDiscovery();
+            if (Build.VERSION.SDK_INT >= 21) {
+                final BluetoothLeScanner scanner = adapter.getBluetoothLeScanner();
+                scanner.startScan(new ScanCallback() {
+                    @Override
+                    public void onScanResult(int callbackType, ScanResult result) {
+                        super.onScanResult(callbackType, result);
 
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(receiver, filter);
+                        BluetoothDevice device = result.getDevice();
+                        if (device != null) {
+                            String deviceName = device.getName();
+                            String deviceAddress = device.getAddress();
+                            int type = device.getType();
+                            BluetoothClass deviceClass = device.getBluetoothClass();
+                            int major = deviceClass.getDeviceClass();
+                            int manor = deviceClass.getMajorDeviceClass();
+                            int hasCode = device.hashCode();
+
+                            if (deviceName!=null && deviceName.contains("Pebble")) {
+                                ParcelUuid[] uuids = device.getUuids();
+                                UUID uuid = uuids[0].getUuid();
+                                try {
+                                    BluetoothServerSocket socket = adapter.listenUsingRfcommWithServiceRecord(deviceName, uuid);
+                                }catch (IOException e) {
+
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onBatchScanResults(List<ScanResult> results) {
+                        super.onBatchScanResults(results);
+
+                        for (int i=0; i<results.size(); i++) {
+                            BluetoothDevice device = results.get(i).getDevice();
+                            if (device != null) {
+                                String deviceName = device.getName();
+                                String deviceAddress = device.getAddress();
+                                int type = device.getType();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onScanFailed(int errorCode) {
+                        super.onScanFailed(errorCode);
+                        Toast.makeText(null, "扫描失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+
+            }
+
         } else {
             Toast.makeText(this, "蓝牙未连接", Toast.LENGTH_SHORT).show();
             //adapter.enable();
